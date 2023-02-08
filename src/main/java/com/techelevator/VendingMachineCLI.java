6b291a0b0a94cf1af38c20b0db0d31da9c057cb8
Hewad Noori <hewadnoori16@gmail.com>
2023-02-08 17:45:47 -0500

    added comments to how the code is working

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -5,25 +5,29 @@ import com.techelevator.view.VendingMenu;
 
 
 public class VendingMachineCLI {
-
+	//Constant string values are defined for various options in the main menu and the purchase menu.
+	//main menu options
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
 	private static final String MAIN_MENU_SECRET_OPTION = "*Sales Report";
-
+	//purchase options
 	private static final String PURCHASE_MENU_OPTION_FEED_MONEY = "Feed Money";
 	private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
 	private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION = "Finish Transaction";
-
+	//Arrays are created for each menu, containing the respective options.
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT, MAIN_MENU_SECRET_OPTION };
 	private static final String[] PURCHASE_MENU_OPTIONS = { PURCHASE_MENU_OPTION_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_FINISH_TRANSACTION};
-
+	//A private member variable of type VendingMenu is defined.
 	private VendingMenu menu;
-
+	//The constructor takes a VendingMenu object as an argument and initializes the private member variable menu.
 	public VendingMachineCLI(VendingMenu menu) {
 		this.menu = menu;
 	}
-
+	//The run method has the main logic for the application. It uses a while loop to keep the program running until the running variable is set to false.
+	// The getChoiceFromOptions method is called on the menu object to get the user's choice.
+		//If the user chooses "Display Vending Machine Items", the corresponding action will be performed (currently nothing).
+		//If the user chooses "Purchase", the corresponding action will be performed (currently nothing).
 	public void run() {
 		boolean running = true;
 		while (running) {
@@ -37,7 +41,8 @@ public class VendingMachineCLI {
 			}
 		}
 	}
-
+	//The main method creates a VendingMenu object and a VendingMachineCLI object, and calls the run method on the VendingMachineCLI object.
+	//This code is just a skeleton for a CLI for a vending machine, we must implement it ourselves.
 	public static void main(String[] args) {
 		VendingMenu menu = new VendingMenu(System.in, System.out);
 		VendingMachineCLI cli = new VendingMachineCLI(menu);
--- src/main/java/com/techelevator/view/VendingMenu.java
+++ src/main/java/com/techelevator/view/VendingMenu.java
@@ -5,16 +5,22 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class VendingMenu {
+public class VendingMenu {	//class provides an interface for a user to interact with a vending machine by displaying a list of options and receiving input from the user.
 
 	private PrintWriter out;
 	private Scanner in;
-
+	//The constructor takes two arguments, input and output, which are the input and output streams for the class.
+	// The constructor initializes the in and out instance variables, which are type Scanner and PrintWriter.
+	// These are used to read input from the user and write output to the user.
 	public VendingMenu(InputStream input, OutputStream output) {
 		this.out = new PrintWriter(output);
 		this.in = new Scanner(input);
 	}
 
+	//The getChoiceFromOptions method takes an array of options as an argument and returns a selected option.
+	// The method calls the displayMenuOptions method to display the options to the user.
+	// Then, it calls the getChoiceFromUserInput method to receive the user's choice.
+	// The method continues to call the getChoiceFromUserInput method until a valid choice has been selected.
 	public Object getChoiceFromOptions(Object[] options) {
 		Object choice = null;
 		while (choice == null) {
@@ -23,7 +29,11 @@ public class VendingMenu {
 		}
 		return choice;
 	}
-
+	//The getChoiceFromUserInput method takes an array of options as an argument and returns a selected option.
+	// The method reads input from the user and tries to parse it as an integer.
+	// If the input is a valid integer, it checks if the integer is within the range of the options array.
+	// If it is, it returns the corresponding option from the array.
+	// If the input isn't a valid integer or is outside the range of the options array, the method returns null and writes an error message to the user.
 	private Object getChoiceFromUserInput(Object[] options) {
 		Object choice = null;
 		String userInput = in.nextLine();
@@ -40,7 +50,9 @@ public class VendingMenu {
 		}
 		return choice;
 	}
-
+	//The displayMenuOptions method takes an array of options as an argument and displays the options to the user.
+	// The method loops through the options array and writes each option, preceded by its index number, to the output stream.
+	// The method also writes a prompt for the user to choose an option.
 	private void displayMenuOptions(Object[] options) {
 		out.println();
 		for (int i = 0; i < options.length; i++) {
